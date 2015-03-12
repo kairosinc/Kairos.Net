2184ddd3c6f4204bd07ec2aadde55f3f26b289de
Dan McGrath <djm158@pitt.edu>
2015-03-12 11:17:34 -0400

    Included error checking functionality

---
+++ Kairos.API/Errors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kairos.API
+{
+    /// <summary>
+    /// Error information 
+    /// </summary>
+    public class Errors
+    {
+        public int ErrCode { get; set; }
+        public string Message { get; set; }
+    }
+
+}
