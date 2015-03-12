e5310371ae96ca62c68f57c2ea7d59f95332ebb3
djm158 <djm158@pitt.edu>
2015-03-12 11:32:31 -0400

    Update KairosClient.cs

--- Kairos.API/KairosClient.cs
+++ Kairos.API/KairosClient.cs
@@ -1,22 +1,3 @@
-﻿/*************************************************************************
-* 
-* 18 SIGNALS CONFIDENTIAL
-* __________________
-* 
-*  [2013] 18 SIGNALS, LLC
-*  All Rights Reserved.
-* 
-* NOTICE:  All information contained herein is, and remains
-* the property of 18 SIGNALS, LLC and its suppliers,
-* if any.  The intellectual and technical concepts contained
-* herein are proprietary to 18 SIGNALS, LLC and its suppliers 
-* and may be covered by U.S. and Foreign Patents, patents in process, 
-* and are protected by trade secret or copyright law.
-* Dissemination of this information or reproduction of this material
-* is strictly forbidden unless prior written permission is obtained
-* from 18 SIGNALS, LLC.
-*/
-
 using RestSharp;
 using RestSharp.Deserializers;
 using System;
